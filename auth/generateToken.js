95e0e40b72bb6557decd3c6cc9cdd79e88327af1
RodBromberg <rod.bromberg@yahoo.com>
2019-06-17 22:51:31 -0400

    oush

--- auth/generateToken.js
+++ auth/generateToken.js
@@ -1,21 +1,25 @@
 const jwt = require("jsonwebtoken");
 
 const secrets = require("./secrets.js");
-
+// importing our secret that were passing 
 module.exports = {
     generateToken
 };
+// exporting our function
 
 function generateToken(user) {
     const payload = {
-        userId: user.id,
+        subject: user.id,
         username: user.username
-            // ...other data
-
+            // only for basic info publicly visible describing
     }
     const options = {
-        expiresIn: '8h',
-    }
-
+            expiresIn: '8h',
+        }
+        // time for the token to maintain authentication 
+        // can add other options to token
     return jwt.sign(payload, secrets.jwtSecret, options)
+
+    // gets the payload/data and also the secret
+    // secret used to decrypt/encrypt
 }
--- config/routes.js
+++ config/routes.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcryptjs')
 const db = require('../database/dbConfig.js')
 const Users = require('../database/dbMethods')
 const jwt = require('jsonwebtoken')
-const generateToken = require('../auth/generateToken.js')
+const { generateToken } = require('../auth/generateToken.js')
 const { authenticate } = require('../auth/authenticate');
 
 module.exports = server => {
@@ -12,43 +12,81 @@ module.exports = server => {
     server.get('/api/jokes', authenticate, getJokes);
 };
 
+
 function register(req, res) {
     // implement user registration
     const creds = req.body;
+    // performs hashing alogirthm 2^13 times includes salts
     const hash = bcrypt.hashSync(creds.password, 13)
+        // setting user password to the hash
     creds.password = hash;
+    // grabbing the DB 'users'
     db('users')
         .insert(creds)
+        // passing the users registered name/pass 
         .then(added => {
             res.status(201).json(added)
         })
+        // succesfully added username/pass
         .catch(err => {
             res.status(400).json(err)
         })
+        // error adding username/pass
 }
+
+// 22 lines of pure comments for carlos
+// function login(req, res) {
+//     // implement user login
+//     let { username, password } = req.body;
+
+//     db("users")
+//         .where({ username })
+//         .first()
+//         .then(user => {
+//             if (user && bcrypt.compareSync(password, user.password)) {
+//                 //Generates token
+//                 const token = Token.generateToken(user);
+//                 res.status(200).json({
+//                     message: `Hi ${user.username}!`,
+//                     token
+//                 });
+//             } else {
+//                 res.status(401).json({ message: "Error with creds" });
+//             }
+//         })
+//         .catch(err => {
+//             res.status(500).json(err);
+//         });
+// }
 
 function login(req, res) {
-    // implement user login
     let { username, password } = req.body;
-
-    Users.findBy({ username })
+    // setting the passed in credentials to username/password
+    Users.login({ username })
+        // using db method to pass username:'login name' 
         .first()
+        // grab first element of the array
         .then(user => {
+            // returning a promise need .then()/.catch()
             if (user && bcrypt.compareSync(password, user.password)) {
-                const token = generateToken(user);
+                // if the password matches the hashed password
+                const token = generateToken(user)
+                    // then generate a token
                 res.status(200).json({
-                    message: `Welcome ${user.username}!`,
-                    token,
-                });
+                        message: `Welcome ${user.username}`,
+                        authToken: token,
+                    })
+                    // successful request, returns username/token
             } else {
-                res.status(401).json({ message: 'Invalid Credentials' });
+                res.status(401).json({ message: 'Invalid Credentials' })
             }
+            // some type of error with the credentials
         })
-        .catch(error => {
-            res.status(500).json(error);
-        });
+        .catch(err => {
+            res.status(500).json({ err, message: 'Possible mispelling of user/pass or other error' })
+        })
+        // Internal server error possibly spelling of username or password not credentials
 }
-
 
 function getJokes(req, res) {
     const requestOptions = {
@@ -57,6 +95,7 @@ function getJokes(req, res) {
 
     axios
         .get('https://icanhazdadjoke.com/search', requestOptions)
+        // getting mediocre jokes from api
         .then(response => {
             res.status(200).json(response.data.results);
         })
